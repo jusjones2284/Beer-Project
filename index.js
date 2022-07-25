f2091742685b722534143a26780eefc5d30eeab5
Justin Jones <jus.jones@yahoo.com>
2022-07-25 08:22:11 -0400

    updated the js

--- index.js
+++ index.js
@@ -1,51 +1,68 @@
 const base_url = 'https://api.punkapi.com/v2/beers'
-// http://localhost:3000/Italian
-
 
 document.addEventListener("DOMContentLoaded", ()=> {
 
 
+function getBeers(){
 
-fetch(base_url)
-.then(response => response.json())
-.then(data =>{
-    console.log(data)
-    const ul = document.getElementById('beer')
-    console.log(ul)
-    data.forEach(beers => {
-        const beerNamesDescrip = `${beers.name} : 
-        ${beers.description}`
-        const li = document.createElement('li')
-        li.innerText = beerNamesDescrip 
-        ul.append(li)
-        li.addEventListener('click',()=>{
+  fetch(base_url)
+  .then(response => response.json())
+  .then(data => data.forEach(beer => showBeer(beer)))}
 
-        
+  getBeers()
 
-        })
-    })
-    
+  
+function showBeer(beer){
 
+const beerCollection = document.getElementById("beer")
+  
+  
+  const div = document.createElement("div")
+  div.classList.add("beverage")
+  const p = document.createElement('p')
+  p.textContent = `${beer.name}`
+
+  const ul = document.createElement('ul')
+  ul.textContent = `${beer.description}`
+  ul.classList.add("beer-descrip")
+  div.append(p)
+  beerCollection.append(div)
+  div.addEventListener('mouseover',function(e){
+  const attachBeerDescrip = ul.textContent
+  div.append(ul)
+} )
+
+}
+const form = document.querySelector(".add-beer-form")
+form.addEventListener("submit", addNewbeer)
+
+function addNewbeer(event){
+  event.preventDefault()
+  const [name] = event.target
+  console.log("Name of beer:", name.value)
+  const beerCollection = document.getElementById("beer")
+  const beerValue = name.value
+  const p = document.createElement("p")
+  p.textContent = beerValue
+  const div =  document.createElement("div")
+  div.classList.add("description")
+  div.append(p)
+  beerCollection.append(div)
+  form.reset()
+
+ 
+}
+
+const allBeverages = document.querySelectorAll(".beverage")
+allBeverages.forEach((div)=>{ 
+  console.log("beers")
 })
 
+document.querySelector('#alert').addEventListener('click', ()=> alert('Thank you for your submission! Drink responsibly.'))
 })
 
+function beerDescription([]){
+}
 
 
 
-
-// const URL = 'http://localhost:3000/pokemons'
-
-// fetch(URL)
-// .then(response => response.json())
-// .then(data => {
-//   console.log(data)
-//   const ul = document.querySelector('')
-//   data.forEach(pokemon =>{
-//     const interpolated = `${pokemon.id} ${pokemon.name}`
-//     const li = document.createElement('li')
-//     li.innerText = interpolated
-//     ul.append(li)
-
-//   })
-// })
